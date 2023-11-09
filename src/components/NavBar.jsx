3b44a5e73d16a339ee4ddef050e4c5e5a223a231
Cassandra Blondez <[cassandra.blondez@gmail.com](mailto:cassandra.blondez@gmail.com)>
2023-11-09 16:10:06 +0100

    add sections and navbar style

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,69 @@
+import { Link, animateScroll as scroll } from "react-scroll";
+import logo from "../assets/logo-portfolio.svg";
+
+function NavBar() {
+  const scrollToTop = () => {
+    scroll.scrollToTop();
+  };
+  return (
+    <div className="navigation">
+      <div className="nav-logo">
+        <img src={logo} alt="Logo lien accueil" onClick={scrollToTop} />
+      </div>
+      <div className="nav-links">
+        <Link
+          activeClass="active"
+          to="section1"
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={500}
+        >
+          Accueil
+        </Link>
+        <Link
+          activeClass="active"
+          to="section2"
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={500}
+        >
+          A propos
+        </Link>
+        <Link
+          activeClass="active"
+          to="section3"
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={500}
+        >
+          Compétences
+        </Link>
+        <Link
+          activeClass="active"
+          to="section4"
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={500}
+        >
+          Projets
+        </Link>
+        <Link
+          activeClass="active"
+          to="section4"
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={500}
+        >
+          Contacts
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
---
+++ src/components/sections/About.jsx
@@ -0,0 +1,9 @@
+function About() {
+  return (
+    <>
+      <div>A propos</div>
+    </>
+  );
+}
+
+export default About;
