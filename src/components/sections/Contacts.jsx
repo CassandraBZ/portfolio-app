477be21dc3d5ef8a0a137fe5237944bbdeaed9ab
Cassandra Blondez <[cassandra.blondez@gmail.com](mailto:cassandra.blondez@gmail.com)>
2023-11-21 10:04:03 +0100

    contact section

--- src/components/sections/Contacts.jsx
+++ src/components/sections/Contacts.jsx
@@ -1,5 +1,23 @@
+import linkedin from "../../assets/linkedin-icon.svg";
+import github from "../../assets/github-icon.svg";
+
 function Contacts() {
-  return <div>Contacts</div>;
+  return (
+    <div className="contact-section">
+      <h1>On reste en contact ?</h1>
+      <div className="contact-content">
+        <p>
+          Vous pouvez m’écrire par email à l’adresse :
+          <a>cassandra.blondez@gmail.com</a>
+        </p>
+        <p>Ou me contacter via les réseaux sociaux :</p>
+        <div className="social">
+          <img src={linkedin} alt="linkedin" />
+          <img src={github} alt="github" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Contacts;
