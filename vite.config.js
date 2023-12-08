0e1cc2a9b934c8a3b5efd436d2bdadadd363288d
Cassandra Blondez <[cassandra.blondez@gmail.com](mailto:cassandra.blondez@gmail.com)>
2023-12-08 15:30:50 +0100

    deploy my app

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/portfolio-app/",
   server: {
     port: 3000,
+  },
+  build: {
+    outDir: "build",
   },
 });
